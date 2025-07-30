0ce808939ad61f71eacbd155823d2a1f3177f879
MounaKrishna <mounakrishna@mindgrovetech.in>
2025-07-30 14:47:56 +0530

    Added a new benchmark. SAD of an image

---
+++ pixel_sad/input_generator.py
@@ -0,0 +1,45 @@
+import random
+
+def generate_sample_sad_blocks():
+    pix1 = [[random.randint(0, 255) for _ in range(16)] for _ in range(16)]
+    pix2 = []
+    for row in pix1:
+        new_row = []
+        for val in row:
+            variation = random.randint(-10, 10)
+            new_val = max(0, min(255, val + variation))
+            new_row.append(new_val)
+        pix2.append(new_row)
+    return pix1, pix2
+
+def to_c_array(name, matrix):
+    c_str = f"unsigned char {name}[16][16] = {{\n"
+    for row in matrix:
+        row_str = ", ".join(str(pixel) for pixel in row)
+        c_str += f"    {{ {row_str} }},\n"
+    c_str += "};\n"
+    return c_str
+
+def compute_sad(pix1, pix2):
+    i_sum = 0
+    for y in range(16):
+        for x in range(16):
+            i_sum += abs(pix1[y][x] - pix2[y][x])
+    return i_sum
+
+pix1, pix2 = generate_sample_sad_blocks()
+sad_value = compute_sad(pix1, pix2)
+c_pix1 = to_c_array("pix1", pix1)
+c_pix2 = to_c_array("pix2", pix2)
+sad_c = f"const int expected_sad = {sad_value};\n"
+
+header_content = "// Generated input pixel data for SAD testing\n" 
+header_content += "typedef uint16_t pixel;\n"
+header_content += c_pix1 + "\n" + c_pix2 + "\n" + sad_c
+
+with open("inputs.h", "w") as file:
+    file.write(header_content)
+
+# Write pix1 and pix2 blocks to console or to files as needed:
+#print(c_pix1)
+#print(c_pix2)
---
+++ pixel_sad/pixel_sad.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include "inputs.h"
+#include "timing.h"
+#include "encoding.h"
+
+static int pixel_sad( pixel *pix1, intptr_t i_stride_pix1,  
+                 pixel *pix2, intptr_t i_stride_pix2 ) 
+{                                                   
+    int i_sum = 0;                                  
+    int lx = 16;
+    int ly = 16;
+    for( int y = 0; y < ly; y++ )                   
+    {                                               
+        for( int x = 0; x < lx; x++ )               
+        {                                     
+            i_sum += abs( pix1[x] - pix2[x] );
+        }                                     
+        pix1 += i_stride_pix1;                
+        pix2 += i_stride_pix2;                
+    }                                         
+    return i_sum;                             
+}
+
+int main() {
+  int iterations = (int) ITERATIONS;
+  int start_cycles, end_cycles;
+  int start_instr, end_instr;
+  int sum;
+
+#if (HPM_ENABLE)
+  start_perf();
+#endif
+  write_csr(0x800, 0x27); //Enable log start
+  start_cycles = read_csr(mcycle);
+  start_instr = read_csr(minstret);
+  write_csr(minstret, 0);
+  write_csr(mcycle, 0);
+  for (int i=0; i<iterations; i++)
+    sum = pixel_sad(&pix1, &pix2, 0, 0);
+  end_cycles = read_csr(mcycle);
+  end_instr = read_csr(minstret);
+  write_csr(0x800, 0x07); //Disable log start
+  if (sum != expected_sad) {
+    printf("Output is wrong!! Expected SUM: %d, Computed SUM: %d", expected_sad, sum);
+    return 0;
+  }
+  printf("Total cycles to execute: %d\n", (end_cycles - start_cycles));
+  printf("Total instructions executed: %d\n", (end_instr - start_instr));
+#if (HPM_ENABLE)
+  stop_perf();
+  print_perf();
+#endif
+}
